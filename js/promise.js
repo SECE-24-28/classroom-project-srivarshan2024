8979fa17ea7d6ec1620b4d53de590ffbb13a4338
srivarshan2024 <srivarshan.r2024cse@sece.ac.in>
2025-12-05 10:32:39 +0530

    feat: implement simple to-do app with task addition, deletion, and completion toggle

---
+++ js/promise.js
@@ -0,0 +1,18 @@
+// const res = fetch("https://jsonplaceholder.typicode.com/users").then(res=>
+// {
+//     return res.json()
+// }
+// ).then(user=>{
+//     console.log(user)
+// })
+// console.log(res) ;
+
+// --------------------------------------------------
+
+let fetch_API = async()=>
+{
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    console.log(data);
+}
+fetch_API();
