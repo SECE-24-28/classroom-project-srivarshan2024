33d77a0601db5c68b881385de578a8d197bc096a
srivarshan2024 <srivarshan.r2024cse@sece.ac.in>
2025-12-05 15:49:38 +0530

    feat: add HTML files for user data fetching and text copying functionality

--- js/promise.js
+++ js/promise.js
@@ -1,11 +1,11 @@
-// const res = fetch("https://jsonplaceholder.typicode.com/users").then(res=>
-// {
-//     return res.json()
-// }
-// ).then(user=>{
-//     console.log(user)
-// })
-// console.log(res) ;
+const res = fetch("https://jsonplaceholder.typicode.com/users").then(res=>
+{
+    return res.json()
+}
+).then(user=>{
+    console.log(user)
+})
+console.log(res) ;
 
 // --------------------------------------------------
 
