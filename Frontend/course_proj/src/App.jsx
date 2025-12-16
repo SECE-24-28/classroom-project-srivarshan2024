8acbe9ce450b2c750c3ad897b7242372cb0e77c3
srivarshan2024 <srivarshan.r2024cse@sece.ac.in>
2025-12-16 09:56:59 +0530

    feat: enhance course management functionality; add edit and delete options for courses, update input placeholders

--- Frontend/course_proj/src/App.jsx
+++ Frontend/course_proj/src/App.jsx
@@ -5,10 +5,10 @@ import { addCourses } from './api/CourseApi'
 import { updateCourses } from './api/CourseApi'
 import { deleteCourses } from './api/CourseApi'
 
-
 function App() {
   const [title, setTitle] = useState("")
   const [duration, setDuration] = useState("")
+  const [editId,setEditId] = useState("")
   const [courses, setCourses] = useState([])
 
   const fetchCourse= async()=>{
@@ -28,22 +28,50 @@ const handleSubmit=async(e)=>{
   setTitle("")
   setDuration("")
 }
+
+const handleDelete=async(id)=>{
+  await deleteCourses(id)
+  fetchCourse()
+  alert("Course Deleted Successfully....")
+}
+
+const handleEdit=(course)=>{
+  setTitle(course.title)
+  setDuration(course.duration)
+  setEditId(course._id)
+}
+
+const update = async()=>{
+  await updateCourses(editId,{title,duration})
+  setTitle("")
+  setDuration("")
+  setEditId("")
+  fetchCourse()
+
+}
+
+
   return (
     <>
       <h1>Course Details</h1>
       <hr />
       <form onSubmit={handleSubmit}>
-        <input type="text"  value={title}
+        <input type="text" placeholder='Course Name' value={title}
           onChange={(e)=> setTitle(e.target.value)} />
-        <input type="text" value={duration}
+        <input type="text" placeholder='Duration' value={duration}
         onChange={(e)=> setDuration(e.target.value)}/>
         <button type="submit">Add Course</button>
+        <button type='button' onClick={update}>update</button>
 
       </form>
       <ul>
         {
           courses.map(course=>
-            <li >{course.title}-{course.duration}</li>
+            <li key={course._id}>
+              {course.title}-{course.duration}-
+              <button onClick={()=>handleEdit(course)}>EDIT</button>
+              <button onClick={()=>handleDelete(course._id)}>Delete</button>
+              </li>
           )
         }
       </ul>
